e20d1d962b3d8b12a6c95b17cd816aa66b45411d
Irwin <ijesperanza@salud.gob.sv>
2022-11-11 07:08:54 -0600

    Vue router

---
+++ vue-roter/src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: () => import("../views/HomeView.vue"),
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/chats",
+      name: "chats",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/ChatsView.vue"),
+      children:[
+        {path:':chatId', component:()=>import('../components/ChatView.vue')}
+      ]
+    },
+  ],
+});
+
+export default router;
---
+++ vue-roter/src/views/ChatsView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <aside>
+      <div v-for="chat in chats" :key="chat.id">
+        <router-link :to="`/chats/${chat.id}`">{{ chat.name }}</router-link>
+      </div>
+    </aside>
+    <article>
+      <router-view />
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    chats: [
+      { id: 1, name: "Ximena" },
+      { id: 2, name: "Daniel" },
+      { id: 3, name: "Miguel" },
+    ],
+  }),
+};
+</script>
